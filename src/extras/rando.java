5349baac9c4b9a1d2e8b2cdcef4086e424dd88e2
codeclub <codeclub@smms-rm23-35.smusd.local>
2018-05-02 14:59:14 -0700

    checkingifinputisanum

---
+++ src/extras/rando.java
@@ -0,0 +1,18 @@
+package extras;
+
+import java.util.Random;
+
+public class rando {
+
+	public static void main(String[] args) {
+		
+		Random craxz = new Random();
+		
+		System.out.println(craxz);
+		
+		
+		
+		
+		
+	}
+}
